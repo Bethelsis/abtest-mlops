a102d5599dfb4fb133e1291814bc6f6e258c5ab8
Bethelsis <Sisaybethelhem@gmail.com>
2021-07-24 12:54:17 +0300

    Update model.py

--- model.py
+++ model.py
@@ -1,3 +1,18 @@
-# import necessary library
+import dvc.api
 import pandas as pd
 import numpy as np
+
+path = 'data/AdSmartABdata.csv'
+repo = '/home/bethelhem/abtest-mlops'
+version = 'v0'
+data_url = dvc.api.get_url(
+  path = path,
+  repo = repo,
+  rev=version
+  )
+
+if name == "main":
+    warnings.filterwarnings("ignore")
+    np.random.seed(60)
+
+    data=pd.read_csv(data_url,sep=",")
